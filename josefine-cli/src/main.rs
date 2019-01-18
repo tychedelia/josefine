3564c136dd8931892f4c674e299c4e436820f4bf
jcm <jmcelwain@gmail.com>
2019-01-17 23:54:03 -0800

    Get parser working for one command.

--- josefine-cli/src/main.rs
+++ josefine-cli/src/main.rs
@@ -3,14 +3,20 @@ extern crate nom;
 
 use std::io;
 use std::str::FromStr;
+use clap::App;
+use clap::Arg;
+use std::str;
+use std::net::SocketAddr;
 
 #[derive(Debug)]
 enum Operation {
     Info,
     Get,
+    Add(SocketAddr),
+    None,
 }
 
-impl FromStr for  Operation {
+impl FromStr for Operation {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Operation, ()> {
@@ -22,34 +28,41 @@ impl FromStr for  Operation {
     }
 }
 
-struct Command {
-    operation: Operation,
-}
+named!(not_space, is_not!(" \t\r\n"));
 
-named!(get_greeting<&str,&str>,
-    tag_s!("hi")
-);
+named!(get_word<&[u8], &str>,
+    map_res!(not_space, str::from_utf8));
 
-named!(get_command<&str, &str>,
-    alt!(tag_s!("info") | tag_s!("get"))
-);
+named!(get_socket_addr<&[u8], SocketAddr>,
+    map_res!(get_word, FromStr::from_str));
+
+named!(add_op<&[u8], Operation>, do_parse!(
+    tag_no_case!("info") >>
+    take_till!(nom::is_digit) >>
+    socket_addr: get_socket_addr >>
+    (Operation::Add(socket_addr))
+));
 
 
 fn main() {
+    let matches = App::new("josefine-cli")
+        .version("0.0.1")
+        .author("jcm")
+        .about("Distributed log in rust.")
+        .arg(Arg::with_name("port")
+            .long("port")
+            .value_name("PORT")
+            .required(true)
+            .default_value("8081")
+            .help("Port to connect to."))
+        .get_matches();
+
     let mut line = String::new();
     loop {
         line.clear();
         match io::stdin().read_line(&mut line) {
             Ok(size) => {
-                let operation = match get_command(&line) {
-                    Ok((remaining, token)) => {
-                        Operation::from_str(token).unwrap()
-                    },
-                    Err(_) => {
-                        println!("TODO: Help text.");
-                        continue
-                    },
-                };
+                let operation = add_op(&line.as_bytes());
                 println!("{:?}", operation);
             }
             Err(e) => {}
@@ -57,3 +70,17 @@ fn main() {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let (_, operation) = add_op(b"info 127.0.0.1:8080\n").unwrap();
+        match operation {
+            Operation::Add(addr) => {}
+            _ => panic!()
+        }
+    }
+}
